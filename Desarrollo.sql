991bb0030ce94d100ba1fc650eecebf76f1d9389
leoneldc <leonelchavez2013@hotmail.com>
2022-09-22 19:13:12 -0600

    se creo la tabla de asignación de modulos a app y scrips para el funcionamiento del modulo de seguridad

--- Desarrollo.sql
+++ Desarrollo.sql
@@ -8,19 +8,26 @@ CREATE TABLE IF NOT EXISTS `tbl_modulos` (
     nombre_modulo VARCHAR(50) NOT NULL,
     descripcion_modulo VARCHAR(150) NOT NULL,
     estado_modulo TINYINT DEFAULT 0,
-    primary key (pk_id_modulos)
+    primary key (`pk_id_modulos`)
 );
 
 DROP TABLE IF EXISTS `tbl_aplicaciones`;
 CREATE TABLE IF NOT EXISTS `tbl_aplicaciones` (
 	pk_id_aplicacion INT AUTO_INCREMENT NOT NULL,
-    fk_id_modulo INT NOT NULL,
     nombre_aplicacion VARCHAR(50) NOT NULL,
     descripcion_aplicacion VARCHAR(150) NOT NULL,
     estado_aplicacion TINYINT DEFAULT 0,
-    primary key (pk_id_aplicacion),
-    FOREIGN KEY (fk_id_modulo) REFERENCES tbl_modulos(pk_id_modulos)
+    primary key (`pk_id_aplicacion`)
 );
+
+DROP TABLE IF EXISTS `tbl_AsignacionModuloAplicacion`;
+CREATE TABLE IF NOT EXISTS `tbl_AsignacionModuloAplicacion` (
+  fk_id_modulos INT NOT NULL,
+  fk_id_aplicacion INT NOT NULL, 
+  PRIMARY KEY (`fk_id_modulos`,`fk_id_aplicacion` ),
+  FOREIGN KEY (`fk_id_modulos`) REFERENCES `tbl_modulos` (`pk_id_modulos`),
+  FOREIGN KEY (`fk_id_aplicacion`) REFERENCES `tbl_aplicaciones` (`pk_id_aplicacion`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
 
 DROP TABLE IF EXISTS `tbl_usuarios`;
 CREATE TABLE IF NOT EXISTS `tbl_usuarios` (
@@ -34,7 +41,6 @@ CREATE TABLE IF NOT EXISTS `tbl_usuarios` (
   estado_usuario TINYINT DEFAULT 0 NOT NULL,
   PRIMARY KEY (`pk_id_usuario`)
 );
--- AGREGAR UN BOOLEANO PARA VER SI ES USUARIO NORMAL O ADMIN TOTAL
 
 DROP TABLE IF EXISTS `tbl_perfiles`;
 CREATE TABLE IF NOT EXISTS `tbl_perfiles` (
@@ -97,26 +103,55 @@ CREATE TABLE IF NOT EXISTS `tbl_bitacoraDeEventos` (
   FOREIGN KEY (`fk_id_aplicacion`) REFERENCES `tbl_aplicaciones` (`pk_id_aplicacion`)
 )ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
 
-/*
+
 -- -----MODULOS
-INSERT INTO Modulos VALUES ('0000', 'A', 'A', 1),('1000', 'B', 'B', 1),('2000', 'C', 'C', 1);
+INSERT INTO `tbl_modulos` VALUES 
+('1000', 'Seguridad', 'Seguridad', 1),
+('2000', 'Logistica', 'Logistica', 1),
+('3000', 'Compras', 'Compras', 1),
+('4000', 'Ventas', 'Ventas', 1),
+('5000', 'Produccion', 'Produccion', 1),
+('6000', 'Nominas', 'Nominas', 1),
+('7000', 'Bancos', 'Bancos', 1),
+('8000', 'Contabilidad', 'Contabilidad', 1);
+
 -- -----APLICACIONES
-INSERT INTO `aplicaciones` VALUES ('1', '0', 'APP1 A', 'A1', '1'), ('2', '0', 'APP2 A', 'A2', '1');
-INSERT INTO `aplicaciones` VALUES ('1001', '1000', 'APP1 A', 'A1', '1'), ('1002', '1000', 'APP2 B', 'A2', '1');
-INSERT INTO `aplicaciones` VALUES ('2001', '2000', 'APP1 B', 'A1', '1'), ('2002', '2000', 'APP2 C', 'A2', '1');
+INSERT INTO `tbl_aplicaciones` VALUES 
+('1000', 'MDI', 'PARA SEGURIDAD', '1'), 
+('2000', 'MDI', 'PARA LOGISTICA', '1'), 
+('3000', 'MDI', 'PARA COMPRAS', '1'), 
+('4000', 'MDI', 'PARA VENTAS', '1'), 
+('5000', 'MDI', 'PARA', '1'), 
+('6000', 'MDI', 'PARA', '1'), 
+('7000', 'MDI', 'PARA', '1'), 
+('8000', 'MDI', 'PARA', '1');
+
+-- -----ASIGNACIÓNES MODULO A APLICACION
+INSERT INTO `tbl_asignacionmoduloaplicacion` VALUES 
+('1000', '1000'), 
+('2000', '2000'), 
+('3000', '3000'), 
+('4000', '4000'), 
+('5000', '5000');
+
 -- -----USUARIOS
-INSERT INTO `Usuarios` VALUES ('1', 'admin', 'admin', 'admin', 'HO0aGo4nM94=', 'esduardo@gmail.com', '2022-07-02 21:00:48', '1');
-INSERT INTO `Usuarios` VALUES ('2', 'leonel', 'dominguez', 'laionel', 'HO0aGo4nM94=', 'laionel@gmail.com', '2022-07-02 21:00:48', '1');
+INSERT INTO `tbl_usuarios` VALUES 
+('1', 'admin', 'admin', 'admin', 'HO0aGo4nM94=', 'esduardo@gmail.com', '2022-07-02 21:00:48', '1'),
+('2', 'leonel', 'dominguez', 'laionel', 'HO0aGo4nM94=', 'laionel@gmail.com', '2022-07-02 21:00:48', '1');
+
 -- -----PERFILES
-INSERT INTO Perfiles VALUES ('1', 'Administrador', 'contiene todos los permisos del programa', 1),('2', 'Tester', 'tiene acceso a ciertas aplicaciones', 1);
+INSERT INTO `tbl_perfiles` VALUES 
+('1', 'Administrador', 'contiene todos los permisos del programa', 1),
+('2', 'Tester', 'tiene acceso a ciertas aplicaciones', 1);
+
 -- -----PERMISOS DE APLICACIONES A PERFILES
-INSERT INTO `PermisosAplicacionPerfil` VALUES ('1', '1001', '1', '1', '1', '1', '1');
-INSERT INTO `PermisosAplicacionPerfil` VALUES ('1', '2001', '1', '1', '1', '1', '1');
--- -----PERMISOS DE APLICACIONES A USUARIOS
-INSERT INTO `PermisosAplicacionesUsuario` VALUES ('1001', '1', '1', '1', '1', '1', '1');
-INSERT INTO `PermisosAplicacionesUsuario` VALUES ('2001', '1', '1', '1', '1', '1', '1');
-INSERT INTO `PermisosAplicacionesUsuario` VALUES ('2001', '2', '1', '1', '1', '1', '1');
+INSERT INTO `tbl_permisosAplicacionPerfil` VALUES 
+('1', '1000', '1', '1', '1', '1', '1'),
+('1', '2000', '1', '1', '1', '1', '1'),
+('1', '3000', '1', '1', '1', '1', '1'),
+('1', '4000', '1', '1', '1', '1', '1'),
+('1', '5000', '1', '1', '1', '1', '1');
+
 -- -----ASIGNACIÓN DE PERFIL A USUARIO
-INSERT INTO `AsignacionesPerfilsUsuario` VALUES ('1', '1');
-INSERT INTO `AsignacionesPerfilsUsuario` VALUES ('2', '1');
-*/
+INSERT INTO `tbl_asignacionesPerfilsUsuario` VALUES 
+('1', '1'),('1', '2');
