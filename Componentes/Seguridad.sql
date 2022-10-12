55cd76f545dc0c4d0838fd2e03fec9d6f30fc322
Joshua53Barrios <jbarrioso3@miumg.edu.gt>
2022-10-11 22:09:41 -0600

    División de componentes y de módulos

---
+++ Componentes/Seguridad.sql
@@ -0,0 +1,222 @@
+DROP DATABASE  IF EXISTS `colchoneria`;
+CREATE DATABASE IF NOT EXISTS `colchoneria`;
+USE `colchoneria`;
+
+DROP TABLE IF EXISTS `tbl_modulos`;
+CREATE TABLE IF NOT EXISTS `tbl_modulos` (
+	pk_id_modulos INT NOT NULL,
+    nombre_modulo VARCHAR(50) NOT NULL,
+    descripcion_modulo VARCHAR(150) NOT NULL,
+    estado_modulo TINYINT DEFAULT 0,
+    primary key (`pk_id_modulos`)
+);
+
+DROP TABLE IF EXISTS `tbl_aplicaciones`;
+CREATE TABLE IF NOT EXISTS `tbl_aplicaciones` (
+	pk_id_aplicacion INT NOT NULL,
+    nombre_aplicacion VARCHAR(50) NOT NULL,
+    descripcion_aplicacion VARCHAR(150) NOT NULL,
+    estado_aplicacion TINYINT DEFAULT 0,
+    primary key (`pk_id_aplicacion`)
+);
+
+DROP TABLE IF EXISTS `tbl_AsignacionModuloAplicacion`;
+CREATE TABLE IF NOT EXISTS `tbl_AsignacionModuloAplicacion` (
+  fk_id_modulos INT NOT NULL,
+  fk_id_aplicacion INT NOT NULL, 
+  PRIMARY KEY (`fk_id_modulos`,`fk_id_aplicacion` ),
+  FOREIGN KEY (`fk_id_modulos`) REFERENCES `tbl_modulos` (`pk_id_modulos`),
+  FOREIGN KEY (`fk_id_aplicacion`) REFERENCES `tbl_aplicaciones` (`pk_id_aplicacion`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
+
+DROP TABLE IF EXISTS `tbl_usuarios`;
+CREATE TABLE IF NOT EXISTS `tbl_usuarios` (
+  pk_id_usuario INT AUTO_INCREMENT NOT NULL,
+  nombre_usuario VARCHAR(50) NOT NULL,
+  apellido_usuario VARCHAR(50) NOT NULL,
+  username_usuario VARCHAR(20) NOT NULL,
+  password_usuario VARCHAR(100) NOT NULL,
+  email_usuario VARCHAR(50) NOT NULL,
+  ultima_conexion_usuario DATETIME NULL DEFAULT NULL,
+  estado_usuario TINYINT DEFAULT 0 NOT NULL,
+  pregunta varchar(50) not null,
+  respuesta varchar(50) not null,
+  PRIMARY KEY (`pk_id_usuario`)
+);
+
+DROP TABLE IF EXISTS `tbl_perfiles`;
+CREATE TABLE IF NOT EXISTS `tbl_perfiles` (
+	pk_id_perfil INT AUTO_INCREMENT NOT NULL,
+    nombre_perfil VARCHAR(50) NOT NULL,
+    descripcion_perfil VARCHAR(150) NOT NULL,
+    estado_perfil TINYINT DEFAULT 0,
+    primary key (pk_id_perfil)
+);
+
+DROP TABLE IF EXISTS `tbl_permisosAplicacionesUsuario`;
+CREATE TABLE IF NOT EXISTS `tbl_permisosAplicacionesUsuario` (
+  fk_id_aplicacion INT NOT NULL, 
+  fk_id_usuario INT NOT NULL, 
+  guardar_permiso BOOLEAN DEFAULT FALSE,
+  modificar_permiso BOOLEAN DEFAULT FALSE,
+  eliminar_permiso BOOLEAN DEFAULT FALSE,
+  buscar_permiso BOOLEAN DEFAULT FALSE,
+  imprimir_permiso BOOLEAN DEFAULT FALSE,
+  PRIMARY KEY (`fk_id_aplicacion`,`fk_id_usuario`),
+  FOREIGN KEY (`fk_id_aplicacion`) REFERENCES `tbl_aplicaciones` (`pk_id_aplicacion`),
+  FOREIGN KEY (`fk_id_usuario`) REFERENCES `tbl_usuarios` (`pk_id_usuario`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
+
+DROP TABLE IF EXISTS `tbl_permisosAplicacionPerfil`;
+CREATE TABLE IF NOT EXISTS `tbl_permisosAplicacionPerfil` (
+  fk_id_perfil INT NOT NULL, 
+  fk_id_aplicacion INT NOT NULL, 
+  guardar_permiso BOOLEAN DEFAULT FALSE,
+  modificar_permiso BOOLEAN DEFAULT FALSE,
+  eliminar_permiso BOOLEAN DEFAULT FALSE,
+  buscar_permiso BOOLEAN DEFAULT FALSE,
+  imprimir_permiso BOOLEAN DEFAULT FALSE,
+  PRIMARY KEY (`fk_id_perfil`,`fk_id_aplicacion`),
+  FOREIGN KEY (`fk_id_aplicacion`) REFERENCES `tbl_aplicaciones` (`pk_id_aplicacion`),
+  FOREIGN KEY (`fk_id_perfil`) REFERENCES `tbl_perfiles` (`pk_id_perfil`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
+
+DROP TABLE IF EXISTS `tbl_asignacionesPerfilsUsuario`;
+CREATE TABLE IF NOT EXISTS `tbl_asignacionesPerfilsUsuario` (
+  fk_id_usuario INT NOT NULL, 
+  fk_id_perfil INT NOT NULL,
+  PRIMARY KEY (`fk_id_usuario`,`fk_id_perfil` ),
+  FOREIGN KEY (`fk_id_usuario`) REFERENCES `tbl_usuarios` (`pk_id_usuario`),
+  FOREIGN KEY (`fk_id_perfil`) REFERENCES `tbl_perfiles` (`pk_id_perfil`)
+) ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
+
+DROP TABLE IF EXISTS `tbl_bitacoraDeEventos`;
+CREATE TABLE IF NOT EXISTS `tbl_bitacoraDeEventos` (
+  pk_id_bitacora INT AUTO_INCREMENT NOT NULL,
+  fk_id_usuario INT NOT NULL,
+  fk_id_aplicacion INT NOT NULL,
+  fecha_bitacora DATE NOT NULL,
+  hora_bitacora TIME NOT NULL,
+  host_bitacora VARCHAR(45) NOT NULL,
+  ip_bitacora VARCHAR(100) NOT NULL,
+  accion_bitacora VARCHAR(25) NOT NULL,
+  PRIMARY KEY (`pk_id_bitacora`),
+  FOREIGN KEY (`fk_id_usuario`) REFERENCES `tbl_usuarios` (`pk_id_usuario`),
+  FOREIGN KEY (`fk_id_aplicacion`) REFERENCES `tbl_aplicaciones` (`pk_id_aplicacion`)
+)ENGINE = InnoDB DEFAULT CHARACTER SET = utf8;
+
+
+/* -- -----MODULOS
+INSERT INTO `tbl_modulos` VALUES
+('1000', 'SEGURIDAD', 'Seguridad', 1),
+('2000', 'LOGISTICA', 'Logistica', 1),
+('3000', 'COMPRAS Y VENTAS', 'Compras y Ventas', 1),
+('5000', 'PRODUCCIÓN', 'Produccion', 1),
+('6000', 'NOMINAS', 'Nominas', 1),
+('7000', 'BANCOS', 'Bancos', 1),
+('8000', 'CONTABILIDAD', 'Contabilidad', 1);
+
+-- -----APLICACIONES
+INSERT INTO `tbl_aplicaciones` VALUES
+('1000', 'MDI SEGURIDAD', 'PARA SEGURIDAD', '1'),
+('2000', 'MDI LOGISTICA', 'PARA LOGISTICA', '1'),
+('3000', 'MDI COMPRAS Y VENTAS', 'PARA COMPRAS Y VENTAS', '1'),
+('5000', 'MDI PRODUCCIÓN', 'PARA PRODUCCIÓN', '1'),
+('6000', 'MDI NOMINAS', 'PARA NOMINAS', '1'),
+('7000', 'MDI BANCOS', 'PARA BANCOS', '1'),
+('8000', 'MDI CONTRABILIDAD', 'PARA CONTRABILIDAD', '1');
+
+-- -----ASIGNACIÓNES MODULO A APLICACION
+INSERT INTO `tbl_asignacionmoduloaplicacion` VALUES
+('1000', '1000'),
+('2000', '2000'),
+('3000', '3000'),
+('5000', '5000'),
+('6000', '6000'),
+('7000', '7000'),
+('8000', '8000');
+
+-- -----USUARIOS
+INSERT INTO `tbl_usuarios` VALUES
+('1', 'admin', 'admin', 'admin', 'HO0aGo4nM94=', 'esduardo@gmail.com', '2022-07-02 21:00:48', '1', 'COLOR FAVORITO', 'ROJO');
+
+-- -----PERFILES
+INSERT INTO `tbl_perfiles` VALUES
+('1', 'ADMINISTRADOR', 'contiene todos los permisos del programa', 1);
+
+-- -----PERMISOS DE APLICACIONES A PERFILES
+INSERT INTO `tbl_permisosAplicacionPerfil` VALUES
+('1', '1000', '1', '1', '1', '1', '1'),
+('1', '2000', '1', '1', '1', '1', '1'),
+('1', '3000', '1', '1', '1', '1', '1'),
+('1', '5000', '1', '1', '1', '1', '1'),
+('1', '6000', '1', '1', '1', '1', '1'),
+('1', '7000', '1', '1', '1', '1', '1'),
+('1', '8000', '1', '1', '1', '1', '1');
+
+-- -----ASIGNACIÓN DE PERFIL A USUARIO
+INSERT INTO `tbl_asignacionesPerfilsUsuario` VALUES
+('1', '1'); */
+
+-- vistas ------------------------------------------------------------------------
+CREATE 
+VIEW `colchoneria`.`vista_aplicacion_perfil` AS 
+    SELECT 
+        `b`.`fk_id_perfil` AS `ID`,
+        `a`.`nombre_perfil` AS `Perfil`,
+        `b`.`fk_id_aplicacion` AS `Aplicacion`,
+        `b`.`guardar_permiso` AS `Insertar`,
+        `b`.`modificar_permiso` AS `Modificar`,
+        `b`.`eliminar_permiso` AS `Eliminar`,
+        `b`.`buscar_permiso` AS `Buscar`,
+        `b`.`imprimir_permiso` AS `Reporte`
+    FROM
+        (`colchoneria`.`tbl_permisosaplicacionperfil` `b`
+        JOIN `colchoneria`.`tbl_perfiles` `a` ON ((`a`.`pk_id_perfil` = `b`.`fk_id_perfil`)));
+        
+CREATE 
+VIEW `colchoneria`.`vista_aplicacionusuario` AS
+    SELECT 
+        `b`.`fk_id_aplicacion` AS `Aplicacion`,
+        `b`.`fk_id_usuario` AS `ID`,
+        `a`.`nombre_usuario` AS `Usuario`,
+        `b`.`guardar_permiso` AS `Insertar`,
+        `b`.`modificar_permiso` AS `Editar`,
+        `b`.`eliminar_permiso` AS `Eliminar`,
+        `b`.`buscar_permiso` AS `Buscar`,
+        `b`.`imprimir_permiso` AS `Reporte`
+    FROM
+        (`colchoneria`.`tbl_permisosaplicacionesusuario` `b`
+        JOIN `colchoneria`.`tbl_usuarios` `a` ON ((`a`.`pk_id_usuario` = `b`.`fk_id_usuario`)));
+        
+CREATE 
+VIEW `colchoneria`.`vista_modulo_aplicacion` AS
+    SELECT 
+        `b`.`fk_id_modulos` AS `ID`,
+        `a`.`nombre_modulo` AS `Modulo`,
+        `b`.`fk_id_aplicacion` AS `Aplicacion`,
+        `c`.`nombre_aplicacion` AS `Nombre`,
+        `c`.`descripcion_aplicacion` AS `Descripcion`
+    FROM
+        ((`colchoneria`.`tbl_asignacionmoduloaplicacion` `b`
+        JOIN `colchoneria`.`tbl_modulos` `a` ON ((`a`.`pk_id_modulos` = `b`.`fk_id_modulos`)))
+        JOIN `colchoneria`.`tbl_aplicaciones` `c` ON ((`c`.`pk_id_aplicacion` = `b`.`fk_id_aplicacion`)));
+        
+CREATE 
+VIEW `colchoneria`.`vista_perfil_usuario` AS
+    SELECT 
+        `b`.`fk_id_usuario` AS `ID`,
+        `a`.`nombre_usuario` AS `Usuario`,
+        `a`.`username_usuario` AS `nickName`,
+        `b`.`fk_id_perfil` AS `perfil`,
+        `c`.`nombre_perfil` AS `Nombre`
+    FROM
+        ((`colchoneria`.`tbl_asignacionesperfilsusuario` `b`
+        JOIN `colchoneria`.`tbl_usuarios` `a` ON ((`a`.`pk_id_usuario` = `b`.`fk_id_usuario`)))
+        JOIN `colchoneria`.`tbl_perfiles` `c` ON ((`c`.`pk_id_perfil` = `b`.`fk_id_perfil`)));
+        
+
+
+
+
+
