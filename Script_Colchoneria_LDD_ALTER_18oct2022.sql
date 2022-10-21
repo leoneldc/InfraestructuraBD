ba25e6a99bd583223eb1ee0728f9ae65f4c1a32e
GeorgeMayenR <georgemayen11520@gmail.com>
2022-10-21 14:01:40 -0600

    Alterando la tabla tblcotizaciones, agregamos una columna llamada fk_codigo_producto

--- Script_Colchoneria_LDD_ALTER_18oct2022.sql
+++ Script_Colchoneria_LDD_ALTER_18oct2022.sql
@@ -1,3 +1,5 @@
 USE `colchoneria`;
 alter table tbl_usuarios add pregunta varchar(50) not null;
 alter table tbl_usuarios add respuesta varchar(50) not null;
+alter table tblcotizaciones add column fk_codigo_producto int;
+alter table tblcotizaciones add constraint fk2_codigo_producto foreign key (fk_codigo_producto ) references tbl_producto(pk_codigo_producto );
